dc98da07749625c50a595d1ed079ae035b3baead
Gabriel-Alexandru Badescu <a-gabade@microsoft.com>
2015-07-29 12:54:34 +0300

    some reorg, still messy + Parallel examples

---
+++ ConsoleApplication2/Parallel.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Security.Cryptography;
+
+namespace ConsoleApplication2
+{
+    class Parallel
+    {
+
+        public static void H2()
+        {
+            string[] values = new string[10000000];
+            string findValue = " 9000000.";
+
+            Console.WriteLine("Building initial array of {0} length...", values.Length);
+
+            for (int j = 0; j < values.Length; j++)
+            {
+                values[j] = string.Format("This Is My Value: {0}.", j.ToString());
+            }
+
+            start:
+
+            Console.WriteLine("Searching for '{0}'...", findValue);
+
+            Console.WriteLine("Running parallel search...");
+
+            DateTime start = DateTime.Now;
+
+            ParallelOptions options = new ParallelOptions();
+            options.MaxDegreeOfParallelism = 4;
+
+            System.Threading.Tasks.Parallel.For(0, values.Length, options, (i) =>
+            {
+                if (values[i].Contains(findValue))
+                {
+                    Console.WriteLine("Found {0}", values[i]);
+                    Console.WriteLine("Search completed in: {0}", DateTime.Now - start);
+                }
+            });
+
+            Console.WriteLine("Parallel for completed in : {0}", DateTime.Now - start);
+
+            Console.WriteLine("Running linear search...");
+
+            start = DateTime.Now;
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i].Contains(findValue))
+                {
+                    Console.WriteLine("Found {0}", values[i]);
+                    Console.WriteLine("Search completed in: {0}", DateTime.Now - start);
+                }
+            }
+
+            Console.WriteLine("Linear for completed in : {0}", DateTime.Now - start);
+
+            Console.WriteLine("Press any key to exit or press 'Z' to do it again...");
+
+            if (Console.ReadKey().Key == ConsoleKey.Z)
+            {
+                Console.WriteLine("");
+                goto start;
+            }
+        }
+       
+
+        public static void H()
+        {
+            Console.WriteLine("loading..");
+
+            //init some stuff
+            List<string> list = new List<string>();
+            //  Stopwatch sw = new Stopwatch();
+            Random r = new Random();
+            double sum = 0;
+            DateTime dt;
+            for (int i = 0; i < 1000000; i++)
+            {
+                list.Add(r.Next(0, 999999).ToString());
+            }
+
+            //non parallel time
+            Console.WriteLine("starting");
+
+            dt = DateTime.Now;
+            foreach (string f in list)
+            {
+                MD5(f);
+            }
+            Console.WriteLine($"non parallel time: {(DateTime.Now - dt).Seconds} seconds");
+
+            //parallel 
+            sum = 0;
+            dt = DateTime.Now;
+
+           list.AsParallel().ForAll(str => MD5(str));
+
+            Console.WriteLine($"parallel time:{ (DateTime.Now - dt).Seconds} seconds");
+
+            Console.WriteLine("end");
+        }
+
+        static string MD5(string password)
+        {
+            // byte array representation of that string
+            byte[] encodedPassword = new UTF8Encoding().GetBytes(password);
+
+            // need MD5 to calculate the hash
+            byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(encodedPassword);
+
+            // string representation (similar to UNIX format)
+            string encoded = BitConverter.ToString(hash)
+               // without dashes
+               .Replace("-", string.Empty)
+               // make lowercase
+               .ToLower();
+            return encoded;
+        }
+    }
+}
--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -6,7 +6,6 @@ using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using static System.Console;
 
 
 namespace ConsoleApplication2
@@ -20,80 +19,23 @@ namespace ConsoleApplication2
 
             Console.WriteLine();
             Console.WriteLine($" method called is: {strRead}()");
-            MethodInfo callMethod = typeof(Program).GetMethod(strRead);
+
+           
+            MethodInfo callMethod = typeof(General).GetMethod(strRead);
+            if (callMethod == null)
+            {
+            callMethod = typeof(Parallel).GetMethod(strRead);
+            }
+
+
             object result = callMethod.Invoke(null, null);
 
             Console.ReadKey();
 
         }
-        public string First { get; } = "Jane";
+     
         
 
-        //async task stuff
-        public static async void D()
-        {
-            long q = await DoStuff();
-            string a = "InsertedString1";
-            string b = "nanaaa";
-            var s = $"{a} is {b}";
-
-            WriteLine($"RESULT: {q} string: {s}");
-        }
-
-      static async Task<long> DoStuff()
-        {
-            long q = 0;
-
-            await Task.Run(() =>
-            {
-                for (int i = 0; i < 30000; i++)
-                {
-                    q += i;
-                    Console.WriteLine("-------------------");
-                }
-                
-            });
-            return q;
-            
-        }
-
-        //serialization/deserialization
-        static public void E()
-        {
-            Account account = new Account
-            {
-             Email = "james@example.com",
-            Active = true,
-            CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
-            Roles = new List<string>
-            {
-            "User",
-           "Admin"
-            }
-            };
-            
-          string json = JsonConvert.SerializeObject(account, Formatting.Indented);
-
-            Account account2 = JsonConvert.DeserializeObject<Account>(json);
-
-            Console.WriteLine($"serialized json {json}");
-            Console.WriteLine($"deserialized data in converted json {account2.Email}");
-        }
-
-        //call direct 
-        public static void F() => Console.WriteLine("Lambda method call");
-
-        //Null-Conditional Operator ('optional' values, a la Swift)
-        static Account testAcc = null;
-        public static void G()
-        {
-            Console.WriteLine("Starting...");
-           //crashes //   Console.WriteLine(testAcc.GetSomeStringMethod());
-            Console.WriteLine(testAcc?.GetSomeStringMethod()); //doesn't print anyting
-            string testStr = testAcc?.GetSomeStringMethod() ?? "INITed String"; // if not, then ( cond ? action : reaction)
-            testAcc = new Account();
-            Console.WriteLine(testAcc?.GetSomeStringMethod());
-        }
     }
 
     public class Account : IDisposable
